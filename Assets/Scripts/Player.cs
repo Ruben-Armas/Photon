daf41df964b672cd020f933b6ab2d229fde0dec1
Ruben-Armas <rubenarmas2010@gmail.com>
2023-11-13 20:14:50 +0000

    Finished - SpriteRotate Sinc

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Photon.Pun;
+using UnityEngine.UIElements;
 
 public class Player : MonoBehaviour
 {
@@ -26,11 +27,14 @@ public class Player : MonoBehaviour
             // Mover jugador
             rb.velocity = (transform.right * speed * Input.GetAxis("Horizontal")) +
                             (transform.up * rb.velocity.y);
-            // Rotar sprite
-            if (rb.velocity.x > 0.1f)
-                GetComponent<SpriteRenderer>().flipX = false;
-            else if (rb.velocity.x < -0.1f)
-                GetComponent<SpriteRenderer>().flipX = true;
+
+            // Rotar sprite - Solo cuando la velocidad es +-0.1 y cuando cambia de dirección
+            if (rb.velocity.x > 0.1f && GetComponent<SpriteRenderer>().flipX)
+                // Remote Procedure Call (función a ejecutar, a quién, parámetro)
+                // El servidor hace que todos los ugadores ejecuten la función pasándole ese parámetro
+                GetComponent<PhotonView>().RPC("RotateSprite", RpcTarget.All, false);
+            else if (rb.velocity.x < -0.1f && !GetComponent<SpriteRenderer>().flipX)
+                GetComponent<PhotonView>().RPC("RotateSprite", RpcTarget.All, true);
 
             // Salto
             if (Input.GetButtonDown("Jump")) {
@@ -41,6 +45,11 @@ public class Player : MonoBehaviour
             anim.SetFloat("velocityX", Mathf.Abs(rb.velocity.x));   //(en valor absoluto)
             anim.SetFloat("velocityY", rb.velocity.y);
         }
-        
+    }
+
+    [PunRPC]    // Indica que la función se va a usar en el Remote Procedure Call
+                // Sincroniza la variable en todas las instancias del juego
+    void RotateSprite(bool rotate) {
+        GetComponent<SpriteRenderer>().flipX = rotate;
     }
 }
